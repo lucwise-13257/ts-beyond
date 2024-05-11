cafe6d41c4e27958b27a6f99a2e537f6c7b7d607
lucwise-13257 <luca_lise@icloud.com>
2024-05-11 00:34:19 -0700

    Holy god

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,11 @@
 "use client";
-import Image from "next/image";
-import Link from "next/link";
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu";
+import { Navbar } from "@/reuseComponents/navbar";
 export default function Home() {
   return (
     <>
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <Link href="https://youtube.com" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Home
-              </NavigationMenuLink>
-            </Link>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+      <Navbar />
       <footer className="flex h-20 w-full items-center px-4">
-        <p className="mx-auto">Footer</p>
+        <p className="mx-auto">Tsunami Beyond 2024</p>
       </footer>
     </>
   );
---
+++ reuseComponents/navbar.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
+export function Navbar() {
+  return (
+      <NavigationMenu className="flex h-16 justify-center items-center min-w-full border-b">
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href="/" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Home
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+  );
+};
